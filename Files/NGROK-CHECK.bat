667855fdb1fd9e02dde3a3535cc9635539b366fb
JustThinker1 <72818525+JustThinker1@users.noreply.github.com>
2021-06-07 01:30:49 +0200

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -9,8 +9,8 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: administrator
 echo Pass: JustThinker1
-curl -O https://raw.githubusercontent.com/JustThinker1/Windows2019RDP-US/main/Files/DisablePasswordComplexity.ps1?token=ARLR6XK4ZHNU2BAW53KBSKDAXVM6E > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/JustThinker1/Windows2019RDP-US/main/Files/FastConfigVPS_v5.1.exe?token=ARLR6XJAW3OOYHEDMJ63EPTAXVM6W > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/JustThinker1/Windows2019RDP-US/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/JustThinker1/Windows2019RDP-US/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
